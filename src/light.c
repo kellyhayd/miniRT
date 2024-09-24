e3247e141b1ef838f1ca870e4a73532c31bb0bce
Kelly Hayd <hayd.kelly@gmail.com>
2024-09-24 08:43:00 -0300

    SEMI-FEATURE: add draft of lighting function

--- src/light.c
+++ src/light.c
@@ -6,7 +6,7 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 23:08:09 by krocha-h          #+#    #+#             */
-/*   Updated: 2024/09/23 23:23:18 by krocha-h         ###   ########.fr       */
+/*   Updated: 2024/09/24 08:41:48 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,12 @@ t_material	material(void)
 {
 	return ((t_material){.color = color(1, 1, 1), .ambient = 0.1, .diffuse = 0.9, .specular = 0.9, .shininess = 200.0});
 }
+
+// t_color	lighting(t_material m, t_light light, t_point position, t_vector eye, t_vector normal)
+// {
+// 	t_color		effective_color;
+// 	t_vector	lightv;
+
+// 	effective_color = color_hadamard(m.color, light.intensity);
+// 	lightv = normalize(tuple_subtract(light.position, position));
+// }
