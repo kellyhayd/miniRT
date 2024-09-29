3cb2244304db573bcf745adeadc504c14a945876
Kelly Hayd <hayd.kelly@gmail.com>
2024-09-29 15:49:28 -0300

    FEATURE: is_shadowed creation and tests; shade_hit draft, tests created, but not used yet

---
+++ src/shadows.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shadows.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 13:04:14 by krocha-h          #+#    #+#             */
+/*   Updated: 2024/09/29 15:45:56 by krocha-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+bool	is_shadowed(t_world w, t_point position)
+{
+	t_vector	v;
+	t_hit		*hit_list;
+	t_hit		*h;
+	double		distance;
+
+	v = tuple_subtract(w.light->position, position);
+	distance = magnitude(v);
+	hit_list = intersect_world(w, ray(position, normalize(v)));
+	h = hit(hit_list);
+	if (h && h->t < distance)
+		return (true);
+	return (false);
+}
+
+// t_color	shade_hit(t_world w, t_comp comps)
+// {
+// 	return (lighting(comps.object.material, *w.light, comp.point, comp.sight));
+// }
--- tests/tests_light.c
+++ tests/tests_light.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tests_light_and_shading.c                          :+:      :+:    :+:   */
+/*   tests_light.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 15:24:42 by krocha-h          #+#    #+#             */
-/*   Updated: 2024/09/26 13:50:27 by krocha-h         ###   ########.fr       */
+/*   Updated: 2024/09/29 13:58:28 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -216,15 +216,17 @@ void	test_lighting_with_the_eye_between_the_light_and_the_surface(int num_test)
 {
 	// ARRANGE
 	t_color		result;
+	t_sight		sight;
+	t_color		expected = color(1.9, 1.9, 1.9);
+
 	t_light		light = point_light(point(0, 0, -10), color(1, 1, 1));
 	t_material	m = material();
 	t_point		position = point(0, 0, 0);
-	t_vector	eyev = vector(0, 0, -1);
-	t_vector	normalv = vector(0, 0, -1);
-	t_color		expected = color(1.9, 1.9, 1.9);
+	sight.eye = vector(0, 0, -1);
+	sight.normal = vector(0, 0, -1);
 
 	// ACT
-	result = lighting(m, light, position, eyev, normalv);
+	result = lighting(m, light, position, sight);
 
 	// ASSERT
 	print_result(num_test, &expected, &result, color_compare_test, print_ko_color);
@@ -235,15 +237,18 @@ void	test_lighting_with_the_eye_between_light_and_surface_eye_offset_45_degrees(
 {
 	// ARRANGE
 	t_color		result;
+	t_sight		sight;
+	t_color		expected = color(1.0, 1.0, 1.0);
+
 	t_light		light = point_light(point(0, 0, -10), color(1, 1, 1));
 	t_material	m = material();
 	t_point		position = point(0, 0, 0);
-	t_vector	eyev = vector(0, sqrt(2) / 2, -sqrt(2) / 2);
-	t_vector	normalv = vector(0, 0, -1);
-	t_color		expected = color(1.0, 1.0, 1.0);
+	sight.eye = vector(0, sqrt(2) / 2, -sqrt(2) / 2);
+	sight.normal = vector(0, 0, -1);
+
 
 	// ACT
-	result = lighting(m, light, position, eyev, normalv);
+	result = lighting(m, light, position, sight);
 
 	// ASSERT
 	print_result(num_test, &expected, &result, color_compare_test, print_ko_color);
@@ -254,15 +259,17 @@ void	test_lighting_with_eye_opposite_surface_light_offset_45_degrees(int num_tes
 {
 	// ARRANGE
 	t_color		result;
+	t_sight		sight;
+	t_color		expected = color(0.7364, 0.7364, 0.7364);
+
 	t_light		light = point_light(point(0, 10, -10), color(1, 1, 1));
 	t_material	m = material();
 	t_point		position = point(0, 0, 0);
-	t_vector	eyev = vector(0, 0, -1);
-	t_vector	normalv = vector(0, 0, -1);
-	t_color		expected = color(0.7364, 0.7364, 0.7364);
+	sight.eye = vector(0, 0, -1);
+	sight.normal = vector(0, 0, -1);
 
 	// ACT
-	result = lighting(m, light, position, eyev, normalv);
+	result = lighting(m, light, position, sight);
 
 	// ASSERT
 	print_result(num_test, &expected, &result, color_compare_test, print_ko_color);
@@ -273,15 +280,17 @@ void	test_lighting_with_eye_in_the_path_of_the_reflection_vector(int num_test)
 {
 	// ARRANGE
 	t_color		result;
+	t_sight		sight;
+	t_color		expected = color(1.6364, 1.6364, 1.6364);
+
 	t_light		light = point_light(point(0, 10, -10), color(1, 1, 1));
 	t_material	m = material();
 	t_point		position = point(0, 0, 0);
-	t_vector	eyev = vector(0, -sqrt(2) / 2, -sqrt(2) / 2);
-	t_vector	normalv = vector(0, 0, -1);
-	t_color		expected = color(1.6364, 1.6364, 1.6364);
+	sight.eye = vector(0, -sqrt(2) / 2, -sqrt(2) / 2);
+	sight.normal = vector(0, 0, -1);
 
 	// ACT
-	result = lighting(m, light, position, eyev, normalv);
+	result = lighting(m, light, position, sight);
 
 	// ASSERT
 	print_result(num_test, &expected, &result, color_compare_test, print_ko_color);
@@ -292,15 +301,17 @@ void	test_lighting_with_light_behind_the_surface(int num_test)
 {
 	// ARRANGE
 	t_color		result;
+	t_sight		sight;
+	t_color		expected = color(0.1, 0.1, 0.1);
+
 	t_light		light = point_light(point(0, 0, 10), color(1, 1, 1));
 	t_material	m = material();
 	t_point		position = point(0, 0, 0);
-	t_vector	eyev = vector(0, 0, -1);
-	t_vector	normalv = vector(0, 0, -1);
-	t_color		expected = color(0.1, 0.1, 0.1);
+	sight.eye = vector(0, 0, -1);
+	sight.normal = vector(0, 0, -1);
 
 	// ACT
-	result = lighting(m, light, position, eyev, normalv);
+	result = lighting(m, light, position, sight);
 
 	// ASSERT
 	print_result(num_test, &expected, &result, color_compare_test, print_ko_color);
@@ -326,10 +337,9 @@ int	main()
 		test_lighting_with_the_eye_between_light_and_surface_eye_offset_45_degrees,	// 14
 		test_lighting_with_eye_opposite_surface_light_offset_45_degrees,			// 15
 		test_lighting_with_eye_in_the_path_of_the_reflection_vector,				// 16
-		test_lighting_with_light_behind_the_surface,								// 17
 	};
 
-	printf("\n%sTESTING LIGHT AND SHANDING:%s\n", YELLOW, RESET);
+	printf("\n%sTESTING LIGHT:%s\n", YELLOW, RESET);
 
 	for (int i = 0; i < sizeof(test_funcs) / sizeof(test_funcs[0]); i++) {
 		test_funcs[i](i + 1);
