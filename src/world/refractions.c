ee02081242321b5925a87a2e5812e71dc5ca560b
DanielSurf10 <71985055+DanielSurf10@users.noreply.github.com>
2024-10-18 16:39:20 -0300

    FEATURE: implemented finding the refracted color under total internal reflection

--- src/world/refractions.c
+++ src/world/refractions.c
@@ -2,10 +2,16 @@
 
 t_color	refracted_color(t_world w, t_comps comps, int depth)
 {
+	double	n_ratio;
+	double	cos_i;
+	double	sin2_t;
 	t_color	ref_color;
 
 	(void) w;
-	if (comps.object.material.transparency == 0 || depth == 0)
+	n_ratio = comps.n1 / comps.n2;
+	cos_i = dot(comps.sight.eye, comps.sight.normal);
+	sin2_t = pow(n_ratio, 2) * (1 - pow(cos_i, 2));
+	if (comps.object.material.transparency == 0 || depth == 0 || sin2_t > 1)
 		ref_color = color(0, 0, 0);
 	else
 		ref_color = color(1, 1, 1);
--- tests/tests_reflection.c
+++ tests/tests_reflection.c
@@ -400,6 +400,36 @@ void	test_the_refracted_color_at_the_maximum_recursive_depth(int num_test)
 	world_clear(&w);
 }
 
+void	test_the_refracted_color_under_total_internal_reflection(int num_test)
+{
+	// ARRANGE
+	t_world	w = default_world();
+
+	t_shape	*shape = w.shape->next;
+	shape->material.transparency = 1.0;
+	shape->material.refractive_index = 1.5;
+
+	t_ray	r = ray(point(0, 0, sqrt(2) / 2), vector(0, 1, 0));
+
+	t_hit	*hit_list = NULL;
+	add_intersection(&hit_list, intersection(-sqrt(2) / 2, *shape));
+	add_intersection(&hit_list, intersection(sqrt(2) / 2, *shape));
+
+	t_comps	comps = prepare_computations(*hit_list->next, r, hit_list);
+	t_color	expected = color(0, 0, 0);
+	t_color	result;
+
+	// ACT
+	result = refracted_color(w, comps, 5);
+
+	// ASSERT
+	print_result(num_test, &expected, &result, color_compare_test, print_ko_color);
+
+	// CLEAR
+	hit_clear_list(&hit_list);
+	world_clear(&w);
+}
+
 int	main(void)
 {
 	void	(*tests[])(int) =
@@ -417,6 +447,7 @@ int	main(void)
 		test_under_point_is_offset_below_the_surface,						// 11
 		test_the_refracted_color_with_an_opaque_surface,					// 12
 		test_the_refracted_color_at_the_maximum_recursive_depth,			// 13
+		test_the_refracted_color_under_total_internal_reflection,			// 14
 	};
 
 	printf("\n%sTESTING REFLECTIONS:%s\n", YELLOW, RESET);
