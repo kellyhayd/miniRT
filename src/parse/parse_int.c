3f8a7de7d7facb60862eda39f5b79be96cf0c259
DanielSurf10 <71985055+DanielSurf10@users.noreply.github.com>
2024-10-25 14:03:01 -0300

    FEATURE: parsing - added error message in parse_int

---
+++ src/parse/parse_int.c
@@ -0,0 +1,25 @@
+#include "minirt.h"
+
+bool	parse_int(char *str, int *num)
+{
+	// OBS: eu não sei se o parse_int precisa receber números negativos também
+	// vou deixar ele só aceitar números positivos
+	// Os negativos só são usados para double
+
+	int	i;
+
+	i = 0;
+	while (str[i] && ft_isdigit(str[i]))
+		i++;
+	if (str[i] != '\0')
+	{
+		ft_putendl_fd(RED "Error!" RESET, 2);
+		ft_putstr_fd("Expected " GREEN "int" RESET
+			", received " GREEN, 2);
+		ft_putstr_fd(str, 2);
+		ft_putendl_fd(RESET, 2);
+		return (false);
+	}
+	*num = ft_atoi(str);
+	return (true);
+}
