4cc3dba502d0699c6847da21aa503f3ec0ceb12e
DanielSurf10 <71985055+DanielSurf10@users.noreply.github.com>
2024-10-28 17:46:36 -0300

    REFACTOR: norminette fix in patterns.c

--- src/materials/patterns.c
+++ src/materials/patterns.c
@@ -30,13 +30,29 @@ void	set_pattern_transformation(t_pattern *pattern, t_matrix transformation)
 	pattern->inverse = inverse(transformation);
 }
 
+static t_color	checkers_shape(t_pattern pattern, t_shape object,
+	t_point pattern_point)
+{
+	double	u;
+	double	v;
+	t_color	color_pattern;
+
+	if (object.shape_type == SPHERE || object.shape_type == CONE
+		|| object.shape_type == CYLINDER)
+	{
+		spherical_map(pattern_point, &u, &v);
+		color_pattern = uv_pattern_at(pattern.map.checkers, u, v);
+	}
+	else
+		color_pattern = checkers_at(pattern, pattern_point);
+	return (color_pattern);
+}
+
 t_color	pattern_at_shape(t_pattern pattern, t_shape object, t_point world_point)
 {
 	t_point	object_point;
 	t_point	pattern_point;
 	t_color	color_pattern;
-	double	u;
-	double	v;
 
 	object_point = mx_multiply_tuple(object.inverse, world_point);
 	pattern_point = mx_multiply_tuple(pattern.inverse, object_point);
@@ -47,17 +63,7 @@ t_color	pattern_at_shape(t_pattern pattern, t_shape object, t_point world_point)
 	else if (pattern.pattern_type == RING)
 		color_pattern = ring_at(pattern, pattern_point);
 	else if (pattern.pattern_type == CHECKERS)
-	{
-		if (object.shape_type == SPHERE || object.shape_type == CONE
-			|| object.shape_type == CYLINDER)
-		{
-			spherical_map(pattern_point, &u, &v);
-			// pattern.map.map_fn(pattern_point, &u, &v);
-			color_pattern = uv_pattern_at(pattern.map.checkers, u, v);
-		}
-		else
-			color_pattern = checkers_at(pattern, pattern_point);
-	}
+		color_pattern = checkers_shape(pattern, object, pattern_point);
 	else
 		color_pattern = pattern.color_a;
 	return (color_pattern);
